ba8e90f2b3e83ca1d4c647f880235909f711d40c
ooi.yanheng@gmail.com <ooiyh@.(none)>
2010-06-22 15:21:47 +0800

    first commit

---
+++ src/head/eye/main/BSteamer.java
@@ -0,0 +1,23 @@
+package head.eye.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.*;
+
+
+public class BSteamer extends Activity {
+    /** Called when the activity is first created. */
+
+	private cameraview cam;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera);
+        
+        cam = new cameraview(this);
+        ((FrameLayout) findViewById(R.id.preview)).addView(cam);
+        
+       
+    }
+}
---
+++ src/head/eye/main/cameraview.java
@@ -0,0 +1,42 @@
+package head.eye.main;
+
+import java.io.IOException;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.Camera;
+import android.hardware.Camera.PreviewCallback;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class cameraview extends SurfaceView implements SurfaceHolder.Callback{
+	public Camera camera;
+	SurfaceHolder mHolder;
+	
+	public cameraview(Context context)
+	{
+		super(context);
+		mHolder = getHolder();
+		mHolder.addCallback(this);
+		mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+	public void surfaceCreated(SurfaceHolder holder) {
+        camera=Camera.open();
+
+        try {
+                camera.setPreviewDisplay(mHolder);
+        }
+        catch (Throwable t) {
+        }
+    }
+
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		camera.stopPreview();
+		camera = null;
+	}
+
+	public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+		camera.startPreview();
+	}
+}
